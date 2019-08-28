58d5ca3544c212da1d2ca82df2f7b62090b5b867
pmeter <54602618+pmeter@users.noreply.github.com>
2019-08-28 17:34:33 +0200

    Update app_conf.py

--- app_conf.py
+++ app_conf.py
@@ -1,7 +1,7 @@
 #Enable static_mode to try the app without a connection to a Bluesound player.
 static_mode_conf = False
 
-#Replace the part "192.168.178.10" with the ip-adress of your Bluesound player.
+#Replace the part "192.168.190.10" with the ip-adress of your Bluesound player.
 base_url_conf = "http://192.168.190.10:11000/"
 
 #If you want to use the app on Windows, replace this with the drive letter of 
